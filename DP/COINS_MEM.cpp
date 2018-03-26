6eb17800455d386e6647f88e18b4858aa8c18291
sKAR04 <cs16btech11010@iith.ac.in>
2018-03-27 04:34:08 +0530

    COINS

---
+++ DP/COINS_MEM.cpp
@@ -0,0 +1,104 @@
+/*
+#####################################################
+# I will win.. maybe not immediately but definitely #
+#####################################################
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//save time
+#define endl '\n'
+typedef long long ll;
+
+//for sorting
+#define all(a) a.begin(),a.end()
+
+//Constants
+#define PI   3.141592653593
+#define MOD  1000000007LL
+#define EPS  0.000000001
+
+//loops
+#define REP(i,n) 	    for(int i=0;i<(n);++i)
+#define FOR(i,a,b)      for(int i=(a);i<(b);++i)
+#define DFOR(i,a,b)     for(int i=(a);i>=(b);--i)
+
+//vectors
+#define vi vector<int>
+#define vll vector<ll>
+#define vii vector<pair<int,int> >
+#define pb 	push_back
+
+//pairs
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+#define mp make_pair
+#define F first
+#define S second
+
+//fast I/O
+#ifndef _WIN32
+#define getchar getchar_unlocked
+#define putchar putchar_unlocked
+#endif
+#define gc getchar
+#define pc putchar
+#define scan getFoo
+
+//If using cin and cout
+#define IOS ios::sync_with_stdio(false)
+#define TIE cin.tie(NULL)
+
+//queue
+#define di deque<int>
+#define qi queue<int>
+#define PQ priority_queue
+
+//general
+#define E empty()
+
+//Input methods
+template <typename T>
+void getFoo(T &x){
+    x=0;
+    register char c=gc();
+    for(;c<48 || c>57;c=gc());
+    for(;c>47 && c<58;c=gc())
+        x=(x<<1)+(x<<3)+c-48;
+}
+
+template <typename T1,typename T2>
+void getFoo(T1 &x,T2 &y){
+    getFoo(x);
+    getFoo(y);
+}
+
+template <typename T1,typename T2,typename T3>
+void getFoo(T1 &x,T2 &y,T3 &z){
+    getFoo(x);
+    getFoo(y);
+    getFoo(z);
+}
+//Declare all variables and methods needed between this comment and the next one(OCD lol)
+map<ll,pair<ll,bool> > dp;
+ll solve(ll n){
+    if(dp[n].S)
+        return dp[n].F;
+    else if(n/2+n/3+n/4>n){
+        dp[n]=mp(solve(n/2)+solve(n/3)+solve(n/4),true);
+        return dp[n].F;
+    }
+    else{
+        dp[n]=mp(n,true);
+        return n;
+    }
+}
+//Main function
+int main(){
+    ll n;
+    while(scanf("%lld",&n)!=EOF)
+        cout<<solve(n)<<endl;
+
+    return 0;
+}
